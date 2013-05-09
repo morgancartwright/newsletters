1d761572879633de74352d5f566fad36966d9807
Morgan Cartwright <mcartwright@digitalfirstmedia.com>
2013-05-09 15:11:16 -0700

    Updates merc config all feeds

--- config/merc_config.php
+++ config/merc_config.php
@@ -8,7 +8,7 @@ $property_title = "San Jose Mercury News";
 $property_url = "mercurynews.com";
 date_default_timezone_set('America/Los_Angeles');
 //IMAGES_ROOT = 'http://extras.bayareanewsgroup.com/images/email';
-$limit = 20; // max number of items that can be displayed
+$limit = 30; // max number of items that can be displayed
 $title = "";
 
 if($topic == 'business') {
@@ -38,8 +38,21 @@ else if($topic == 'roadshow'){
 	$feedInfo = array("http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268907.xml"=>5, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268908.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268909.xml"=>2);
 	$title = "San Mateo Times";
 } else if ($topic == 'all') {
-	$feedInfo = array("http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/225007.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200223.xml"=>2,
-"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200732.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200222.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200733.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/209705.xml"=>2, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200953.xml"=>2);
+	$feedInfo = array(
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200734.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200748.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/209701.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/225007.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/213301.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/206801.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268200.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268201.xml"=>1, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200223.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200777.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200764.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268907.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268908.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/268909.xml"=>1,
+"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200732.xml"=>1, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200222.xml"=>1, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200733.xml"=>1, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/209705.xml"=>1, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200953.xml"=>1);
 	$title = "All Feeds";
 } else { // defaults to news
 	$feedInfo = array("http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200734.xml"=>3,"http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200748.xml"=>3, "http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/209701.xml"=>3);
